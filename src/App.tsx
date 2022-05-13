e4d8917fbf5b3a24ac8223d165ac8e79219c5d4f
Takahiro Hirata <takakun47@gmail.com>
2022-05-13 19:21:28 +0900

    add: sample code

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "@grapecity/spread-sheets-resources-ja";
 import { SpreadSheets } from "@grapecity/spread-sheets-react";
 import "@grapecity/spread-sheets/styles/gc.spread.sheets.excel2013white.css";
 
-import logo from "./logo.svg";
 import "./App.css";
 // 日本語カルチャを設定します
 GC.Spread.Common.CultureManager.culture("ja-jp");
@@ -14,23 +13,22 @@ function App() {
     height: "600px",
   };
 
-  function initSpread(spread: any) {} // eslint-disable-line
+  function initSpread(spread: GC.Spread.Sheets.Workbook) {
+    const sheet = spread.getSheet(0);
+    // テキストを設定します。
+    sheet.setText(0, 0, "This is a text");
+    // 数値を設定します。
+    sheet.setValue(1, 0, 2245);
+    sheet.setValue(2, 0, 2245);
+    // 日付型の値を設定します。
+    sheet.setValue(3, 0, new Date(2020, 10, 7));
+    sheet.getRange(3, 0, 1, 1).formatter("mm-dd-yyyy");
+
+    // 数式を設定します。（A2とA3セルの合計を計算します。）
+    sheet.setFormula(3, 3, "=SUM(A2:A3)");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <SpreadSheets
         hostStyle={hostStyle}
         workbookInitialized={(spread) => initSpread(spread)}
